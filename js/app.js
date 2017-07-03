9d5787b8a61f7067eb1a6c87d7d0e1449a395870
Danube Phan <dphan@devonway.com>
2017-07-02 21:08:13 -0700

    update routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 var app = angular.module('multipageApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'index.html'
+		})
+		.when('/about', {
+			templateUrl: 'views/about.html'
+		})
+		.when('/works', {
+			templateUrl: 'views/works.html'
+		})
+		.otherwise ({
+			redirectTo: '/'
+		})
+})
